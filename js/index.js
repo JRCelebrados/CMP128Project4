cc9ffaf0df1768b8665f17873782d62ab5f8bfab
JRC <jrcelebrados@gmail.com>
2021-05-20 23:18:01 -0400

    Added js tuff

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.getElementById("submit").addEventListener("click", formValidation);
+
+function formValidation() {
+
+  let passWord = document.getElementById("passWord").value;
+
+  console.log(passWord);
+
+}
